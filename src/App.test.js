96c7333d1090b0386919337940b822d66fdc922f
Mukul_Anaspure <x21118141@student.ncirl.ie>
2024-11-07 15:47:46 +0000

    changing version of docker and dependencies

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
 test('shows a new message when the button is clicked', () => {
